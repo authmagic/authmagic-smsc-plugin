b875205e9f322f2c51dad1d9cc7c1743e1fe2baa
oleks <oleksandrknyga@gmail.com>
2018-05-09 13:23:14 +0300

    sms plugin and plugins refactoring

--- plugin.js
+++ plugin.js
@@ -1,11 +1,11 @@
-const _ = require('lodash');
+const path = require('path');
 const smsc = require('node-smsc');
 const pluginName = 'authmagic-smsc-plugin';
 
 module.exports = function ({user, redirectUrl, params, ekey, config:iconfig}) {
   const pluginConfig = iconfig ? iconfig.params ? iconfig.params[pluginName] : null : null;
   const smscInstance = smsc(pluginConfig);
-  const mes = require(`./static/${pluginName}/template.js`)({user, redirectUrl, params, ekey});
+  const mes = require(path.resolve(`./static/${pluginName}/template.js`))({user, redirectUrl, params, ekey});
   smscInstance.send({
     phones: user,
     mes: mes,
