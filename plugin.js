1f10927f261280cf9175cd61c02954c407f591ec
oleks <oleksandrknyga@gmail.com>
2018-10-31 13:11:34 +0200

    add bitly

--- plugin.js
+++ plugin.js
@@ -3,29 +3,57 @@ const querystring = require('querystring');
 const fetch = require('node-fetch');
 const _ = require('lodash');
 const shorturl = require('shorturl-2');
+const { BitlyClient } = require('bitly');
+const bitly = require('bitly');
 const pluginName = 'authmagic-smsc-plugin';
 
 const getParams = ({config:iconfig}) => {
   return iconfig ? iconfig.params ? iconfig.params[pluginName] : null : null;
 };
 
-const getMessage = (options, template) => {
+// TODO do not console.log
+const shortify = async function(uri, provider, params) {
+  switch(provider.toLowerCase()) {
+    case 'bit.ly':
+    case 'bitly':
+      const bitly = new BitlyClient(params.apiKey, {});
+      let result = {};
+      try {
+        result = await bitly.shorten(uri);
+      } catch(e) {
+        throw e;
+      }
+      return result.url;
+    default:
+      return new Promise((resolve, reject) => {
+        shorturl(uri, provider, params, function(result) {
+          if(_.isString(result)) {
+            resolve(result);
+          } else {
+            reject(result);
+          }
+        });
+      })
+  }
+};
+
+const getMessage = async (options, template) => {
   const {ekey, redirectUrl, securityKey} = options;
-  const {shortUrl} = getParams(options);
-  const url = `${redirectUrl}?ekey=${encodeURIComponent(ekey)}`;
-  if(shortUrl.isTurnedOn) {
-    return new Promise((resolve, reject) => {
-      shorturl(url, shortUrl.provider, shortUrl.params, function(result) {
-        if(_.isString(result)) {
-          resolve(template({...options, url: result, securityKey}));
-        } else {
-          reject(result);
-        }
-      });
-    })
+  const {shortUrl: {isTurnedOn, provider, params}} = getParams(options);
+  const uri = `${redirectUrl}?ekey=${encodeURIComponent(ekey)}`;
+  if(isTurnedOn) {
+    let shortUri;
+    try {
+      shortUri = await shortify(uri, provider, params);
+    } catch(e) {
+      // Error propagation doesn't work on node 8
+      throw e;
+    }
+
+    return template({securityKey, url: shortUri, params});
   }
 
-  return new Promise(resolve => resolve(template({securityKey})));
+  return template({securityKey, params});
 };
 
 module.exports = function (options, template, action) {
